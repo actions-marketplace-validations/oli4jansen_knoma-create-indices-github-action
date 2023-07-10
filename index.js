53fcbda2e4b4566e73ed935ce7bcfa663e69bc09
Olivier Jansen <olivier@spinque.com>
2023-07-10 14:48:31 +0200

    -

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
+
+'use strict'
+
 const core = require('@actions/core');
-const github = require('@actions/github');
+const { promises: fs } = require('fs');
+const { join, extname } = require('path');
 
-try {
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
 
-  core.notice('Something happened that you might want to know about.')
-} catch (error) {
-  core.setFailed(error.message);
+const main = async () => {
+  const dataDirectory = join(process.env['GITHUB_WORKSPACE'], 'data');
+
+  const fileNames = await fs.readdir(dataDirectory);
+
+  const filenameToIdentifier = {};
+  const identifierToTypes = {};
+
+  for (const fileName of fileNames) {
+    if (extname(fileName) === '.jsonld') {
+      const filePath = join(dataDirectory, fileName);
+      const fileContent = await fs.readFile(filePath, 'utf8');
+      const document = JSON.parse(fileContent);
+
+      if (!('@id' in document)) {
+        continue;
+      }
+
+      const identifier = document['@id'];
+
+      filenameToIdentifier[fileName] = identifier;
+      identifierToTypes[identifier] = Array.isArray(document['@type']) ? document['@type'] : [document['@type']];
+    }
+  }
+
+  const filenameToIdentifierIndexPath = join(process.env['GITHUB_WORKSPACE'], '_knoma', 'indices', 'filename_identifier.json');
+  const identifierToTypesIndexPath = join(process.env['GITHUB_WORKSPACE'], '_knoma', 'indices', 'identifier_types.json');
+  await fs.writeFile(filenameToIdentifierIndexPath, JSON.stringify(filenameToIdentifier, undefined, 2), {encoding:'utf8',flag:'w'})
+  await fs.writeFile(identifierToTypesIndexPath, JSON.stringify(identifierToTypes, undefined, 2), {encoding:'utf8',flag:'w'})
 }
+
+main().catch(err => {
+  console.error(err);
+  core.setFailed(err.message);
+})
